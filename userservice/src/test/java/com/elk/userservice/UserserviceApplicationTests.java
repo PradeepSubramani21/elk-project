e5aff42a1d7dd2087f07cd2233275cf775e3722a
PradeepSubramani21 <contactpradeepsubramani@gmail.com>
2023-05-30 21:40:07 +0530

    Fixing logging

--- userservice/src/test/java/com/elk/userservice/UserserviceApplicationTests.java
+++ userservice/src/test/java/com/elk/userservice/UserserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.elk.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UserserviceApplicationTests {
 
 	@Test
